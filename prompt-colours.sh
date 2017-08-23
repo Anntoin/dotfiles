e5f9e8da52b134278aa4bb2d4ce275d8fd457e44
Anntoin Wilkinson <anntoin@gmail.com>
2017-08-23 13:07:49 +0100

    Git prompt and prompt colours helper

---
+++ prompt-colours.sh
@@ -0,0 +1,90 @@
+# Note bold colours are escaped versions
+# Reset
+Color_Off='\e[0m'       # Text Reset
+NONE="\[\033[0m\]"
+
+# Regular Colors
+Black='\e[0;30m'        # Black
+BLACK="\[\033[0;30m\]"
+Red='\e[0;31m'          # Red
+RED="\[\033[0;31m\]"
+Green='\e[0;32m'        # Green
+GREEN="\[\033[0;32m\]"
+Yellow='\e[0;33m'       # Yellow
+BROWN="\[\033[0;33m\]"
+Blue='\e[0;34m'         # Blue
+BLUE="\[\033[0;34m\]"
+Purple='\e[0;35m'       # Purple
+PURPLE="\[\033[0;35m\]"
+Cyan='\e[0;36m'         # Cyan
+CYAN="\[\033[0;36m\]"
+White='\e[0;37m'        # White
+LGRAY="\[\033[0;37m\]"
+
+# Bold
+BBlack='\e[1;30m'       # Black
+DGRAY="\[\033[1;30m\]"
+BRed='\e[1;31m'         # Red
+LRED="\[\033[1;31m\]"
+BGreen='\e[1;32m'       # Green
+LGREEN="\[\033[1;32m\]"
+BYellow='\e[1;33m'      # Yellow
+YELLOW="\[\033[1;33m\]"
+BBlue='\e[1;34m'        # Blue
+LBLUE="\[\033[1;34m\]"
+BPurple='\e[1;35m'      # Purple
+LPURPLE="\[\033[1;35m\]"
+BCyan='\e[1;36m'        # Cyan
+LCYAN="\[\033[1;36m\]"
+BWhite='\e[1;37m'       # White
+WHITE="\[\033[1;37m\]"
+
+# Underline
+UBlack='\e[4;30m'       # Black
+URed='\e[4;31m'         # Red
+UGreen='\e[4;32m'       # Green
+UYellow='\e[4;33m'      # Yellow
+UBlue='\e[4;34m'        # Blue
+UPurple='\e[4;35m'      # Purple
+UCyan='\e[4;36m'        # Cyan
+UWhite='\e[4;37m'       # White
+
+# Background
+On_Black='\e[40m'       # Black
+On_Red='\e[41m'         # Red
+On_Green='\e[42m'       # Green
+On_Yellow='\e[43m'      # Yellow
+On_Blue='\e[44m'        # Blue
+On_Purple='\e[45m'      # Purple
+On_Cyan='\e[46m'        # Cyan
+On_White='\e[47m'       # White
+
+# High Intensity
+IBlack='\e[0;90m'       # Black
+IRed='\e[0;91m'         # Red
+IGreen='\e[0;92m'       # Green
+IYellow='\e[0;93m'      # Yellow
+IBlue='\e[0;94m'        # Blue
+IPurple='\e[0;95m'      # Purple
+ICyan='\e[0;96m'        # Cyan
+IWhite='\e[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\e[1;90m'      # Black
+BIRed='\e[1;91m'        # Red
+BIGreen='\e[1;92m'      # Green
+BIYellow='\e[1;93m'     # Yellow
+BIBlue='\e[1;94m'       # Blue
+BIPurple='\e[1;95m'     # Purple
+BICyan='\e[1;96m'       # Cyan
+BIWhite='\e[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\e[0;100m'   # Black
+On_IRed='\e[0;101m'     # Red
+On_IGreen='\e[0;102m'   # Green
+On_IYellow='\e[0;103m'  # Yellow
+On_IBlue='\e[0;104m'    # Blue
+On_IPurple='\e[0;105m'  # Purple
+On_ICyan='\e[0;106m'    # Cyan
+On_IWhite='\e[0;107m'   # White
