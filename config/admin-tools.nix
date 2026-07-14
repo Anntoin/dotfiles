91f8a6f194b77f13cbfb701f962e4f4da867f634
Anntóin Wilkinson <anntoin@gmail.com>
2026-07-14 10:58:39 +0100

    feat(btop): install btop with programs.btop

--- config/admin-tools.nix
+++ config/admin-tools.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    btop
     trippy
     lnav
     drill
     prettyping
     wireshark
   ];
+
+  # A monitor of resources
+  # https://github.com/aristocratos/btop
+  programs.btop = {
+    enable = true;
+    settings = {
+      vim_keys = "true";
+    };
+  };
 }
