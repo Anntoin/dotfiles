583e9db13b1bf2df44a353c1f764ecf8d9e5d103
Anntóin Wilkinson <anntoin@gmail.com>
2026-08-11 20:30:43 +0100

    chore(files): Add comments and links to upstream projects

--- config/files/default.nix
+++ config/files/default.nix
@@ -2,9 +2,20 @@
 # File tools
 {
   home.packages = with pkgs; [
+    # very fast grep
+    # https://github.com/BurntSushi/ripgrep
     ripgrep
+
+    # Unified querying, transformation, and modification of JSON, TOML, YAML, XML, INI, HCL, KDL and CSV
+    # https://daseldocs.tomwright.me/
     dasel
+
+    # Painless compression and decompression in the terminal
+    # https://github.com/ouch-org/ouch
     ouch
+
+    # A universal document converter
+    # https://pandoc.org/
     pandoc
   ];
 }
